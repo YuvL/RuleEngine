69447244708005af1e6a6b1275af6414d6c3a623
Yuri Vlasov <you@example.com>
2017-12-03 22:44:15 +0300

    Http to HttpClient

--- src/board/RuleEngine.Board/ClientApp/app/app.module.shared.ts
+++ src/board/RuleEngine.Board/ClientApp/app/app.module.shared.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
