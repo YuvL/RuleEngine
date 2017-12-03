ea184cd1442fc3a5f92ea9d58d73d9a4930cb8c7
Yuri Vlasov <you@example.com>
2017-12-03 22:24:23 +0300

    added board project

---
+++ src/service/RuleEngine.Core.Abstractions/Services/IRuleEngineService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RuleEngine.Core.Abstractions.Services
+{
+    public interface IRuleEngineService
+    {
+        Task<object> InvokeAsync(string name, object arg);
+    }
+}
